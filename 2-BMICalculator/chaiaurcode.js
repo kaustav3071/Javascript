8f836521cdb0b5f6d323571c6f9eec43ebb6fd33
kaustav3071 <kaustavdas758@gmail.com>
2025-01-17 19:38:01 +0530

    2_bmi js

---
+++ 2-BMICalculator/chaiaurcode.js
@@ -0,0 +1,35 @@
+const form
+= document.querySelector('form')
+
+form.addEventListener('submit',function(e){
+  e.preventDefault()
+  const height = parseInt(document.querySelector('#height').value)
+
+  const weight = parseInt(document.querySelector('#weight').value)
+
+  const result = document.querySelector('#results')
+
+  if (height === '' || height<=0 || isNaN(height)){
+    result.innerHTML = `Please give valid height ${height}`
+  }
+  else if (weight === '' || weight<=0 || isNaN(weight)){
+    result.innerHTML = `Please give valid weight ${weight}`
+  }
+  else{
+    const bmi= (weight/((height*height)/10000)).toFixed(2)
+    if (bmi<18.6){result.innerHTML=
+      result.innerHTML = `<span>${bmi}</span><pre>
+Under Weight</pre>`}
+    else if (bmi>=18.6 && bmi<24.9){
+      result.innerHTML = `<span>${bmi}</span><pre>
+Normal Range</pre>`
+    }
+    else if (bmi>24.9){
+      result.innerHTML = `<span>${bmi}</span><pre>
+Over Weight</pre>`
+    }
+  }
+
+
+})
+
