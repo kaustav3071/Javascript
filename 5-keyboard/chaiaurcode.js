0a6c7cceae5d2fad24d74dde596248c72e2fea6c
Kaustav Das <156155730+kaustav3071@users.noreply.github.com>
2025-01-20 22:56:13 +0530

    Update chaiaurcode.js

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,25 @@
-console.log('Project 5');
+const reference = document.querySelector("#insert")
+
+window.addEventListener('keydown',(e)=>
+{
+  reference.innerHTML=`
+  <table>
+  <thead>
+    <tr>
+      <th>Key</th>
+      <th>Key Code</th>
+      <th>Code</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>${e.key === " "?"Space":e.key}</td>
+      <td>${e.keyCode}</td>
+      <td>${e.code}</td>
+    </tr>
+  </tbody>
+</table>
+
+
+  `
+})
